6e17b8b83e2d9e06e92ac1ba8129621c9dee116d
Santosh Desiraju <desirajusantosh@gmail.com>
2015-03-13 23:42:33 -0500

    Update Missing_number.c

--- Missing_number.c
+++ Missing_number.c
@@ -4,4 +4,27 @@ int missing_element(int arr[], int size){
   int xarr = 0, xacc = 0;
   
   //min and max values in the array
+  min = arr[0];
+  max = arr[0];
+  for (i = 0; i < size; ++i){
+    if(arr[i] > max)
+      max = arr[i];
+    if(arr[i] < min)
+      min = arr[i];
+  }
+  
+  //Xor of all the values 
+  for(i = min; i <= max; ++i){
+    xacc ^= i;
+  }
+  
+  for(i = 0; i < size; ++i){
+    xarr ^= arr[i];
+  }
+  return(xacc ^ xarr);
+}
+
+int main(){
+  
+}
   
