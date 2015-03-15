baa2eb7b887faebc79e44bf959992d25bb7b224e
Santosh Desiraju <desirajusantosh@gmail.com>
2015-03-14 22:48:02 -0500

    Update prime_number_check.c

--- prime_number_check.c
+++ prime_number_check.c
@@ -1,1 +1,22 @@
+void PrimeNumber () {
+  int num;
+  printf (“Enter a number: “);
+  scanf (“%d”, num);
+// 1 and even numbers are not prime
+if ((num == 1) || (num%2 == 0)) {
+  printf (“Number is not prime”);
+  return;
+}
+
+for (int i = 3; i < squareroot (num); i=i+2) {
+   if (num % i == 0) {
+     printf (“Number is not prime”);
+     return;
+   }
+}
+
+printf (“Number is prime”);
+return;
+}
+
 
