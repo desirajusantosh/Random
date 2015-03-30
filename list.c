627f42b3fbb29dc2aaa841456428e32a49016f64
Santosh Desiraju <desirajusantosh@gmail.com>
2015-03-30 13:57:43 -0500

    Update list.c

--- list.c
+++ list.c
@@ -1,2 +1,6 @@
 //reverse a list
 //Reverse a linked list with constant space.
+How to detect a loop in a linked list
+have two pointers moving faster and one pointer moving one after other. 
+If the slow pointer meets any of the faster pointer, then the list is circular. If the list is not circular, 
+either the fast or slow pointer will hit a NULL and we can use that as a condition to exit the function
