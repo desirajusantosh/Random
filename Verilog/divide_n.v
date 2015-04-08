9614dc4d5f6a2d26b81ce8e261e017fd22b12d55
Santosh Desiraju <desirajusantosh@gmail.com>
2015-04-07 22:55:13 -0500

    Update divide_n.v

--- Verilog/divide_n.v
+++ Verilog/divide_n.v
@@ -23,9 +23,10 @@ end
 assign d = qbar;
 endmodule
 
+
 module divide2(q, qbar, d, clk, reset);
 output q;
-output q';
+output qbar;
 input d;
 input clk;
 input reset;
@@ -43,7 +44,7 @@ begin
   qbar = ~q;
   end
 end
-assign d <= qbar;
+assign d = qbar;
 endmodule
 
 
@@ -57,7 +58,48 @@ wire q, qbar1;
 
 always@(posedge clk)
 begin
-dff1 div1(q, qbar1, clk1, reset1);
-dff2 div2(qout, qbarout,  q, reset2);
+dff div1(q, qbar1, clk1, reset1);
+dff div2(qout, qbarout,  q, reset2);
+
+endmodule
+
+
+module dff_6(q, qbar, d, clk, reset);
+output q;
+output qbar;
+input d;
+input clk;
+input reset;
+
+reg q;
+reg qbar;
+
+always@(posedge clk)
+begin
+  if (reset)
+    q = 0;
+    qbar = ~q;
+  else
+  q = d;
+  qbar = ~q;
+  end
+end
+endmodule
+
+
+module divide6(qout, qbarout, clock, rst);
+output qout;
+output qbarout;
+input clock;
+input reset;
+
+wire q, qbar1, qbar2;
+wire qout, qbarout;
+
+always@(posedge clk)
+begin
+dff_6 dv1(qbarout, qbar1, clk, rst);
+dff_6 dv2(qout, qbarout,  clk, rst);
+dff_6 dv3(q6, qbar6, clk, rst);
 
 endmodule
